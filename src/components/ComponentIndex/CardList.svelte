b25c59c9b22cf850b442d75491765a57f02fc0ed
Uneven Coder <ameenshafeeq2005@gmail.com>
2020-09-15 15:42:52 +0300

    responsive

--- src/components/ComponentIndex/CardList.svelte
+++ src/components/ComponentIndex/CardList.svelte
@@ -13,7 +13,7 @@
   }
   .grid {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     grid-gap: 25px;
   }
   .list {
