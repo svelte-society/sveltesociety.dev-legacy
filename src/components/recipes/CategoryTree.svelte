2a07fffe7320888e54cb55d9533180121c90befb
Charlie Moore <zygenhideo@gmail.com>
2020-09-25 17:35:01 -0700

    Reverse formatting changes introduced by my autoformatter

--- src/components/recipes/CategoryTree.svelte
+++ src/components/recipes/CategoryTree.svelte
@@ -1,35 +1,35 @@
 <script>
-    export let nodes
-    import { page } from '@sveltech/routify'
+  export let nodes;
+  import { page } from "@sveltech/routify";
 </script>
 
 <style>
-    ul {
-        list-style: none;
-        margin: 0 0 0 1rem;
-    }
-    li::before {
-        content: '•';
-        color: var(--svelte-grey-light);
-        display: inline-block;
-        width: 1em;
-        margin-left: -1em;
-    }
-    li.active::before {
-        color: var(--svelte-grey);
-    }
-    li.active a {
-        font-weight: bold;
-    }
+  ul {
+    list-style: none;
+    margin: 0 0 0 1rem;
+  }
+  li::before {
+    content: "•";
+    color: var(--svelte-grey-light);
+    display: inline-block;
+    width: 1em;
+    margin-left: -1em;
+  }
+  li.active::before {
+    color: var(--svelte-grey);
+  }
+  li.active a {
+    font-weight: bold;
+  }
 </style>
 
 <ul>
-    {#each nodes.filter((r) => !r.path.includes('/index')) as node}
-        <li class:active={$page.path.includes(node.path)}>
-            <a href={node.path}>{node.meta.frontmatter.title}</a>
-        </li>
-        {#if node.children}
-            <svelte:self nodes={node.children} />
-        {/if}
-    {/each}
+  {#each nodes.filter(r => !r.path.includes('/index')) as node}
+    <li class:active={$page.path.includes(node.path)}>
+      <a href={node.path}>{node.meta.frontmatter.title}</a>
+    </li>
+    {#if node.children}
+      <svelte:self nodes={node.children} />
+    {/if}
+  {/each}
 </ul>
