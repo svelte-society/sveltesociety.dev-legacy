0e785b9956d22390476f0f62242c0f6e0ed9a8f7
MacFJA <macfja@gmail.com>
2020-11-21 22:10:42 +0100

    Add tag filtering

--- src/pages/components/index.svelte
+++ src/pages/components/index.svelte
@@ -5,6 +5,7 @@
   import components from "./components.json";
 
   let searchValue;
+  let searchTag;
   const tags = Array.from(new Set(components.map(item => item.tags).flat()))
   let filterTag = []
 
@@ -14,9 +15,17 @@
 
   $: testData = components.filter(component => {
     if (!searchValue && filterTag.length === 0) return true
-    return searchValue && (component.title.includes(searchValue) || component.description.includes(searchValue))
-      || filterTag.length > 0 && intersection(filterTag, component.tags).length > 0
+
+    if (
+          (searchValue && !(component.title.includes(searchValue) || component.description.includes(searchValue)))
+          || (filterTag.length > 0 && intersection(filterTag, component.tags).length === 0)
+    ) {
+      return false
+    }
+
+    return true
   });
+  $: tagSearchResult = searchTag ? tags.filter(item => item.includes(searchTag)) : tags
   $: categories = Array.from(new Set(testData.map(item => item.category)))
 </script>
 
@@ -51,13 +60,67 @@
     top: 100%;
     right: 0;
   }
+
+  ul.popin {
+    padding: 0.5ex;
+    margin: 0;
+    font-size: 0.7em;
+    max-height: 50vh;
+    overflow-y: auto;
+  }
+
+  ul.popin li {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    text-transform: capitalize;
+  }
+  ul.popin li:hover {
+    background: #eee;
+    border-radius: 3px;
+  }
+  ul.popin li.tag-search {
+    position: sticky;
+    top: -0.5ex;
+    margin: 0 -0.5ex;
+    padding: 0.5ex;
+    border-radius: 4px;
+    background: white;
+  }
+  ul.popin li.tag-search input {
+    margin: 0;
+    background: #f3f6f9;
+  }
+  ul.popin li.tag-search:hover {
+    background: white;
+  }
+
+  ul.popin li label {
+    display: flex;
+    align-items: center;
+    line-height: 0.7rem;
+    padding: 0.8ex;
+  }
+
+  ul.popin li input {
+    flex: 0;
+    margin: 0 1ex 0 0;
+  }
 </style>
 
 <main>
   <h1>This page is not complete yet, below are just placeholders.</h1>
   <div class="controls">
     <div class="inputs">
-      <Button>Tags</Button>
+      <Button active={filterTag.length > 0}>
+        Tags {#if filterTag.length > 0}<small>({filterTag.length})</small>{/if}
+        <ul slot="menu" role="menu" class="popin">
+          <li class="tag-search"><input bind:value={searchTag} /></li>
+          {#each tagSearchResult as tag}
+            <li><label><input type="checkbox" bind:group={filterTag} value={tag}> {tag}</label></li>
+          {/each}
+        </ul>
+      </Button>
       <Button>Size</Button>
       <Button>Other Options</Button>
     </div>
