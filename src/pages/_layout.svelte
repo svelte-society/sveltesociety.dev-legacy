b072e5b0e899134580520335ff8c96d977e62f3e
Dietrich <dietrich.poensgen@heg-portal.de>
2021-01-17 11:52:13 +0100

    add responsivness to footer

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -69,6 +69,20 @@
     max-width: unset;
     padding: 3em;
   }
+
+  @media only screen and (max-width: 768px) {
+    footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      max-width: unset;
+      padding: 2em;
+      font-size: 15px;
+    }
+    a.ghlink{
+      font-size: 15px;
+    }
+  } 
 </style>
 
 <div class="shaded" id="title">
@@ -91,11 +105,12 @@
 <slot />
 
 <footer class="shaded">
-  <div>© Svelte Society {year}</div>
+  <div>&copy; Svelte Society {year}</div>
 
   <div>
     Want to contribute? Pick up an issue on
     <a
+    class="ghlink"
       href="https://github.com/svelte-society/sveltesociety.dev"
       target="_blank">GitHub</a>!
   </div>
