4e432f60fc5ebf2fb61c4f7ae5bd3a702e145447
Dietrich <dietrich.poensgen@heg-portal.de>
2021-02-01 21:21:57 +0100

    update introduction and add discord icon

--- src/components/Introduction.svelte
+++ src/components/Introduction.svelte
@@ -1,10 +1,44 @@
 <script>
-  import { page } from "@roxi/routify";
+  import Icon from "@/components/Icon.svelte";
 </script>
+
+<section>
+  <h2>
+    Welcome to the community page of SvelteJS! Here you can find a lot of
+    information around this great opportunity to build cybernetically enhanced
+    web apps. We provide topics like Svelte events, the local communities around
+    the world and a cheatsheet.
+  </h2>
+  <p>Here you can find more interesting things about us in Social Media:</p>
+  <div class="wrapper">
+    <div class="wrapper-element">
+      <Icon name="youtube" width="7rem" height="7rem" />
+      <a href="https://youtube.com/SvelteSociety" rel="noopener">
+        Our YouTube Channel</a
+      >
+    </div>
+    <div class="wrapper-element">
+      <Icon name="twitter" width="7rem" height="7rem" />
+      <a href="https://twitter.com/sveltesociety" rel="noopener"
+        >Our Twitter for the latest news around the community</a
+      >
+    </div>
+    <div class="wrapper-element">
+      <Icon name="discord" width="7rem" height="7rem" />
+      <a href="https://discord.gg/daNtanDmsE" rel="noopener"
+        >Our Discord to <s>share memes</s> get help</a
+      >
+    </div>
+  </div>
+  <br />
+  <img src="introduction.svg" alt="Live Conversation Application Preview" />
+</section>
 
 <style>
   section {
-    overflow: hidden;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
   }
   h2,
   p {
@@ -12,26 +46,31 @@
     text-align: left;
     margin: 40px 0;
   }
+
+  p {
+    font-size: larger;
+    letter-spacing: 1px;
+  }
+  div.wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+  div.wrapper-element {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-right: 1rem;
+    margin-left: 1rem;
+  }
+  a {
+    color: black;
+    font-weight: lighter;
+  }
+  img {
+    height: 384;
+    width: 806px;
+    margin: 2rem;
+  }
 </style>
-
-<section>
-  <h2>
-    This page is under HEAVY construction and we need your help to make it ready
-    for prime-time! If you're interested in helping out checkout the
-    <a
-      href="https://github.com/svelte-society/sveltesociety.dev"
-      target="_blank" rel="noopener">
-      GitHub repository.
-    </a>
-    You can also come talk to us on the
-    <a href="https://discord.gg/FFYDNp5" target="_blank" rel="noopener">Svelte discord</a>
-    if you want further guidance.
-  </h2>
-
-  <p>
-    In the meantime, you can checkout
-    <a href="https://sveltesummit.com" rel="noopener">Svelte Summit</a>, 
-    our previous online conference. It is free and we have all of the speaker's recorded talks available.
-  </p>
-  <img src="introduction.svg" alt="Live Conversation Application Preview" />
-</section>
