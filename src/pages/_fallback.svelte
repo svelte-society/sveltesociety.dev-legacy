76a8ca892445fb78d82f6eb470a6328b5a7def95
Geoff Rich <geoff.rich@alaskaair.com>
2021-02-10 10:34:20 -0800

    Fix broken layout on 404 page

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,24 +1,22 @@
 <script>
-    import { url } from '@roxi/routify'
+    import { url, metatags } from '@roxi/routify';
+    metatags.title = 'Page not found';
 </script>
 
 <style>
   .huge {
-    font-size: 12rem;
+    font-size: 10rem;
   }
   .e404 {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
     text-align: center;
+    margin-bottom: 1rem;
   }
 </style>
 
 <div class="e404">
-  <div class="huge">404</div>
+  <h1 class="huge">404</h1>
   <div class="big">Page not found. 
-  <!-- link to the parent folder of _fallback.svelte -->
-  <a href={$url('./')}>Go back</a>
+    <!-- link to the parent folder of _fallback.svelte -->
+    <a href={$url('/')}>Go back</a>
   </div>
 </div>
